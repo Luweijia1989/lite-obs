6aaa78ca66d30bd7e8a13f83d1baf011e4fecc79
luweijia <luweijia@bixin.cn>
2023-09-07 19:10:08 +0800

    gs_subsystem: glMapbufferRange to glMapBuffer on MAC

--- source/graphics/gl_helpers.cpp
+++ source/graphics/gl_helpers.cpp
@@ -25,11 +25,8 @@ bool update_buffer(GLenum target, GLuint buffer, const void *data, size_t size)
     if (!gl_bind_buffer(target, buffer))
         return false;
 
-    /* glMapBufferRange with these flags will actually give far better
-     * performance than a plain glMapBuffer call */
-    ptr = glMapBufferRange(target, 0, size,
-                           GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
-    success = gl_success("glMapBufferRange");
+    ptr = glMapBuffer(target, GL_WRITE_ONLY);
+    success = gl_success("glMapBuffer");
     if (success && ptr) {
         memcpy(ptr, data, size);
         glUnmapBuffer(target);
--- source/graphics/gs_texture.cpp
+++ source/graphics/gs_texture.cpp
@@ -180,11 +180,7 @@ bool gs_texture::gs_texture_map(uint8_t **ptr, uint32_t *linesize)
     if (!gl_bind_buffer(GL_PIXEL_UNPACK_BUFFER, d_ptr->unpack_buffer))
         goto fail;
 
-#ifdef PLATFORM_MOBILE
-    *ptr = (uint8_t *)glMapBufferRange(GL_PIXEL_UNPACK_BUFFER, 0, d_ptr->size, GL_MAP_WRITE_BIT);
-#else
     *ptr = (uint8_t *)glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
-#endif
     if (!gl_success("glMapBuffer"))
         goto fail;
 
